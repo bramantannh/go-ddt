524b3407e86311b0d9b482ef0a3271c06d44ed34
Bramanta Nararya Nurul Haq <bramanta.n.n.haq@GDPL-LTP-889.local>
2022-12-13 20:51:39 +0700

    adjust payment module

--- discount.go
+++
@@ -1,5 +0,0 @@
-package discount
-
-func discount(price float32, disc float32) float32 {
-	return price * disc
-}
---
+++ payment.go
@@ -0,0 +1,28 @@
+package payment
+
+type Paymentmenthod int64
+
+const (
+	Creditcard Paymentmenthod = iota
+	Debitcard
+	Virtualaccount
+	Ewallet
+)
+
+func pay(method Paymentmenthod, loggedin bool) string {
+	if !loggedin {
+		return "Not Logged In"
+	}
+	switch method {
+	case Creditcard:
+		return "Paid with credit card"
+	case Debitcard:
+		return "Paid with debit card"
+	case Virtualaccount:
+		return "Paid with virtual account"
+	case Ewallet:
+		return "Paid with ewallet"
+	}
+
+	return "Payment failed"
+}
