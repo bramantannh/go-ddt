cef74183b9041ed3b3b9a1cac0a996fd8b891ef4
Bramanta Nararya Nurul Haq <bramanta.n.n.haq@GDPL-LTP-889.local>
2022-12-13 20:52:04 +0700

    add ddt payment

---
+++ payment_ddt_test.go
@@ -0,0 +1,27 @@
+package payment
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPay(t *testing.T) {
+	tests := []struct {
+		method		Paymentmenthod
+		loggedin	bool
+		expected	string
+	}{
+		{method: Creditcard, loggedin: false, expected: "Not Logged In"},
+		{method: Creditcard, loggedin: true, expected: "Paid with credit card"},
+		{method: Debitcard, loggedin: true, expected: "Paid with debit card"},
+		{method: Virtualaccount, loggedin: true, expected: "Paid with virtual account"},
+		{method: Ewallet, loggedin: true, expected: "Paid with ewallet"},
+	}
+
+	for _, tc := range tests {
+		result := pay(tc.method, tc.loggedin)
+		if !reflect.DeepEqual(tc.expected, result) {
+			t.Errorf("expected \"%s\", but got \"%s\"", tc.expected, result)
+		}
+	}
+}
